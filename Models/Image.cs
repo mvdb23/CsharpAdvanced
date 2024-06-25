e91b7ff0b655960e609bcaec66f5372233147922
mvdb23 <80385548+mvdb23@users.noreply.github.com>
2024-06-25 03:37:54 +0200

    Changed all models and controllers
    Changed all models and controllers to be according to weekopdracht3 criteria.

---
+++ Models/Image.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CsharpAdvanced23_24.Models
+{
+    public class Image
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Url { get; set; }
+
+        public bool IsCover { get; set; }
+    }
+}
---
+++ Models/Reservation.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CsharpAdvanced23_24.Models
+{
+    public class Reservation
+    {
+        [Key]
+        public int Id { get; set; }
+        public Location Location { get; set; } // Two-way 1-to-many relationship with Location
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public Customer Customer { get; set; } // Two-way 1-to-many relationship with Customer
+
+        public float Discount { get; set; }
+    }
+}
